07828aaa3c42dcb4b9842e03a804e0170cad644c
D3FT99 <63921825+D3FT99@users.noreply.github.com>
2024-05-15 12:42:54 +0100

    migrate to v3

--- background.js
+++ background.js
@@ -1,12 +1,23 @@
-function search(select) {
-    var selectionText = select.selectionText;
-    chrome.openTab
-}
-
-chrome.contextMenus.create (
-    {
-        id: '1',
-        title: 'Search on youtube \"%s\"',
-        contexts: ['selection']
-    }
-)
+function loadContextMenus() {
+    chrome.contextMenus.removeAll(() => {
+      chrome.contextMenus.create({
+        id: "contextMenu1",
+        title: 'Search youtube for "%s"',
+        contexts: ["selection"],
+      });
+    });
+  }
+  
+  loadContextMenus();
+  
+  chrome.contextMenus.onClicked.addListener((item, tab) => {
+    const selectedText = item.selectionText;
+    const url = new URL(
+      `https://www.youtube.com/results?search_query=` + selectedText
+    );
+    chrome.tabs.create({
+      url: url.href,
+      index: tab.index + 1,
+    });
+  });
+  
